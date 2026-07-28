27a05327bcec7d4576b85aa6b1d1446df4d78350
yanghong7 <yanghong7@users.noreply.github.com>
2026-07-28 20:50:18 +0800

    Update 2026-07-28 20:50

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,7 @@ git add -A
 git commit --allow-empty -m "Update $(date '+%Y-%m-%d %H:%M')"
 git push origin source
 
-echo "==> 部署 (main 分支)..."
-cd public
-git checkout main
-git add -A
-git commit --allow-empty -m "Site updated: $(date '+%Y-%m-%d %H:%M:%S')"
-git push origin main
+echo "==> 部署静态页面到 main 分支..."
+hexo deploy
 
 echo "==> 完成"
